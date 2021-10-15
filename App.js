80cb2846bbe173db6958503206c7dfb69e90b358
camelcrush <dkxm0927@naver.com>
2021-10-15 22:57:08 +0900

    Preloading Font & Promise

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import AppLoading from 'expo-app-loading';
 import { Image, Text } from 'react-native';
 import { Asset } from 'expo-asset';
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
 
 const cacheImages = images => 
   images.map(image => {
@@ -12,13 +14,17 @@ const cacheImages = images =>
     }
   });
 
+const cacheFonts = fonts => fonts.map(font => Font.loadAsync(font));
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
   const handleFinish = () => setIsReady(true);
   const loadAssets = async () =>{
     const images = [require("./assets/loadingBg.jpeg"), "http://logok.org/wp-content/uploads/2014/07/airbnb-logo-belo-219x286.png"];
-    cacheImages(images);
+    const fonts = [Ionicons.font];
+    const imagePromises = cacheImages(images);
+    const fontPromises = cacheFonts(fonts);
+    return Promise.all([...imagePromises, ...fontPromises]);
   };
   return isReady ? (<Text> I'm Ready</Text>) : (
     <AppLoading onError={console.error} onFinish={handleFinish} startAsync={loadAssets} />
