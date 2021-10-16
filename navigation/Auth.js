a4d2542c1ac11755a0cef0c7f2daee3c27edab95
camelcrush <dkxm0927@naver.com>
2021-10-16 23:28:27 +0900

    Auth Navigation 2

--- navigation/Auth.js
+++ navigation/Auth.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { View, Platform } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
 import Welcome from "../screens/Welcome";
 import SignIn from "../screens/SignIn";
 import SignUp from "../screens/SignUp";
 
 const Auth = createStackNavigator();
 
+const isAndroid = Platform.OS === "android";
+
 export default () => (
-  <Auth.Navigator>
+  <Auth.Navigator
+    screenOptions={{
+      presentation: "modal",
+      headerBackTitleVisible: false,
+      headerTransparent: true,
+      headerBackImage: () => (
+        <View style={{ paddingLeft: 10 }}>
+          <Ionicons name="chevron-down" size={28} />
+        </View>
+      ),
+    }}
+  >
     <Auth.Screen name="Welcome" component={Welcome} />
     <Auth.Screen name="SignIn" component={SignIn} />
     <Auth.Screen name="SignUp" component={SignUp} />
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-export default () => (
+export default ({ navigation }) => (
   <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
     <Text>Welcome</Text>
+    <Button onPress={() => navigation.navigate("SignUp")} title={"Sign Up"} />
+    <Button onPress={() => navigation.navigate("SignIn")} title={"Sign In"} />
   </View>
 );
