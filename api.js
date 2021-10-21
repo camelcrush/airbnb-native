bae0a8fc499c84462626e1be7ed0919a8060e2d7
camelcrush <dkxm0927@naver.com>
2021-10-21 23:01:26 +0900

    Toggle Favs Part 2 & Token Fix

--- api.js
+++ api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import styled from "styled-components";
 
 const callApi = async (method, path, data, jwt) => {
   const headers = {
-    Authorization: jwt,
+    Authorization: `Bearer ${jwt}`,
     "Content-Type": "application/json",
   };
   const baseUrl = "http://127.0.0.1:8000/api/v1";
@@ -19,4 +20,6 @@ export default {
   login: (form) => callApi("post", "/users/login/", form),
   rooms: (page = 1) => callApi("get", `/rooms/?page=${page}`),
   favs: (id) => callApi("get", `/users/${id}/favs/`),
+  toggleFavs: (userId, roomId, token) =>
+    callApi("put", `/users/${userId}/favs/`, { pk: roomId }, token),
 };
