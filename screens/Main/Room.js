60974b2dffe9b6b41a61eebc0edf9645dc538080
camelcrush <dkxm0927@naver.com>
2021-10-23 21:58:12 +0900

    Get Params & RoomPhotos for RoomDetail

--- screens/Main/Room.js
+++ screens/Main/Room.js
@@ -1,19 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components/native";
+import RoomPhotos from "../../components/RoomPhotos";
 
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
+const Container = styled.ScrollView``;
 
 const Text = styled.Text``;
 
-export default ({ route: { params } }) => {
-  console.log(params);
+export default ({ route: { params }, navigation }) => {
+  useEffect(() => {
+    navigation.setOptions({ title: params.name });
+  }, []);
   return (
     <Container>
-      <Text>Room</Text>
+      <RoomPhotos photos={params.photos} />
     </Container>
   );
 };
