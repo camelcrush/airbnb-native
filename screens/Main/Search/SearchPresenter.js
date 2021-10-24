47c79da5f64cb99c62b3ca6dd4a727598b0145e7
camelcrush <dkxm0927@naver.com>
2021-10-24 19:05:03 +0900

    Search Screen Part 2

--- screens/Main/Search/SearchPresenter.js
+++ screens/Main/Search/SearchPresenter.js
@@ -29,17 +29,66 @@ const CancelContainer = styled.TouchableOpacity``;
 
 const CancelText = styled.Text``;
 
+const FiltersContainer = styled.ScrollView`
+  flex-direction: row;
+  margin-top: 10px;
+  padding: 5px;
+`;
+
+const FilterContainer = styled.View`
+  align-items: center;
+  margin-right: 15px;
+`;
+
+const FilterLabel = styled.Text`
+  font-size: 12px;
+  margin-bottom: 5px;
+  font-weight: 500;
+`;
+
+const Filter = styled.TextInput`
+  padding: 10px;
+  background-color: white;
+  border-radius: 20px;
+  width: 80px;
+  shadow-color: #000;
+  shadow-opacity: 0.8;
+  shadow-offset: 0px 1px;
+`;
+
 export default () => {
   const navigation = useNavigation();
   return (
     <DissmissKeyboard>
       <Container>
         <SearchContainer>
-          <SearchBar autoFocus={true} />
+          <SearchBar autoFocus={true} placeholder="Search by City..." />
           <CancelContainer onPress={() => navigation.goBack()}>
             <CancelText>Cancel</CancelText>
           </CancelContainer>
         </SearchContainer>
+        <FiltersContainer
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddigVertical: 10, paddingHorizontal: 20 }}
+        >
+          <FilterContainer>
+            <FilterLabel>Beds</FilterLabel>
+            <Filter placeholder="0" keyboardType={"number-pad"} />
+          </FilterContainer>
+          <FilterContainer>
+            <FilterLabel>Bedrooms</FilterLabel>
+            <Filter placeholder="0" keyboardType={"number-pad"} />
+          </FilterContainer>
+          <FilterContainer>
+            <FilterLabel>Bathrooms</FilterLabel>
+            <Filter placeholder="0" keyboardType={"number-pad"} />
+          </FilterContainer>
+          <FilterContainer>
+            <FilterLabel>Max. Price</FilterLabel>
+            <Filter placeholder="0" keyboardType={"number-pad"} />
+          </FilterContainer>
+        </FiltersContainer>
       </Container>
     </DissmissKeyboard>
   );
