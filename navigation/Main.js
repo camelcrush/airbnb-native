958a5fd1a4a3d1e559fe74dac06bf8aa12b5a30a
camelcrush <dkxm0927@naver.com>
2021-10-24 15:11:09 +0900

    Styling RoomDetail Part 1

--- navigation/Main.js
+++ navigation/Main.js
@@ -10,6 +10,8 @@ import utils from "../utils";
 import colors from "../colors";
 import Room from "../screens/Main/Room";
 import BackBtn from "../components/Auth/BackBtn";
+import { BlurView } from "expo-blur";
+import { StyleSheet } from "react-native";
 
 const TabsNavigator = createBottomTabNavigator();
 
@@ -68,6 +70,19 @@ export default () => (
       component={Tabs}
       options={{ headerShown: false }}
     />
-    <MainNavigator.Screen name="RoomDetail" component={Room} />
+    <MainNavigator.Screen
+      name="RoomDetail"
+      component={Room}
+      options={{
+        headerTransparent: true,
+        headerBackground: () => (
+          <BlurView
+            intensity={100}
+            tint="light"
+            style={StyleSheet.absoluteFill}
+          />
+        ),
+      }}
+    />
   </MainNavigator.Navigator>
 );
